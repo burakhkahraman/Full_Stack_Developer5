24781f4452369025f22c56675381433b8d94683b
Burak Kahraman <burakhasankahraman@gmail.com>
2024-05-02 09:57:54 +0300

    Initial commit

---
+++ src/main/java/com/techcareer/FullStackDeveloper5Application.java
@@ -0,0 +1,13 @@
+package com.techcareer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackDeveloper5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackDeveloper5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/techcareer/FullStackDeveloper5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techcareer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackDeveloper5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
