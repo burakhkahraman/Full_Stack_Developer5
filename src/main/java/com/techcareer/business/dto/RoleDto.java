2e2ef66b0b502dbae1aa8404ef0f3e6e4909575a
Burak Kahraman <burakhasankahraman@gmail.com>
2024-05-06 09:51:54 +0300

    Role DTO

---
+++ src/main/java/com/techcareer/business/dto/RoleDto.java
@@ -0,0 +1,33 @@
+package com.techcareer.business.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.Serializable;
+import java.util.Date;
+
+// LOMBOK
+@Data // @Setter @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Log4j2
+@Builder
+//@SneakyThrows
+// RoleDto
+public class RoleDto implements Serializable {
+
+    // Role ID
+    private Long roleId;
+
+    // Role Name
+    // Validation
+    @NotEmpty(message = "{role.name.validation.constraints.NotNull.message}")
+    private String roleName;
+
+    // System Created Date
+    private Date systemCreatedDate;
+} //end class RoleDto
