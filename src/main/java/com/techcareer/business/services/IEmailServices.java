fc716212faf300d80ee582d27cd1916973db8e20
Burak Kahraman <burakhasankahraman@gmail.com>
2024-05-09 11:55:39 +0300

    EmailApi

--- src/main/java/com/techcareer/business/services/IEmailServices.java
+++ src/main/java/com/techcareer/business/services/IEmailServices.java
@@ -11,6 +11,8 @@ package com.techcareer.business.services;
 // D => Dto
 // E => Entity
 
+import com.techcareer.business.dto.EmailDto;
+
 // Email
 public interface IEmailServices<D, E> {
 
@@ -24,6 +26,11 @@ public interface IEmailServices<D, E> {
     public D mailDatabase(D d);
 
 
+    ///////////////////////////////////////////////////////////////////////////////////////
+    //**** EMAIL DATABASE *****************************************************************//
+    // Email Database
+    EmailDto mailDatabase(EmailDto emailDto);
+
     ////////////////////////////////////////////
     // EMAIL BASIC SEND (text)
     public D basicSendEmail(D d);
@@ -31,4 +38,4 @@ public interface IEmailServices<D, E> {
     // EMAIL INTERMEDIA ATTACHMENT SEND (image,word,text,files)
     public D intermediaSendEmail(D d);
 
-}// end IRegisterService
+}// end IEmailServices
--- src/main/java/com/techcareer/business/services/impl/EmailServicesImpl.java
+++ src/main/java/com/techcareer/business/services/impl/EmailServicesImpl.java
@@ -6,11 +6,14 @@ import com.techcareer.business.dto.EmailDto;
 import com.techcareer.business.services.IEmailServices;
 import com.techcareer.data.entity.EmailEntity;
 import com.techcareer.data.repository.IEmailRepository;
+import com.techcareer.exception.HamitMizrakException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 // LOMBOK
 @RequiredArgsConstructor
@@ -47,15 +50,42 @@ public class EmailServicesImpl implements IEmailServices<EmailDto, EmailEntity> 
     // Email Database
     @Override
     public EmailDto mailDatabase(EmailDto emailDto) {
-        return null;
+        EmailEntity emailEntity1=dtoToEntity(emailDto);
+        EmailEntity emailEntity2= iEmailRepository.save(emailEntity1);
+        EmailDto emailDto2=entityToDto(emailEntity2);
+        emailDto2.setId(emailEntity2.getId());
+        return emailDto2;
+        // 2.YOL (Kısa Olanı)
+        //return entityToDto(iEmailRepository.save(dtoToEntity(emailDto)));
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////
     //**** EMAIL SEND BASIC*****************************************************************//
     // Email Basic Send
     @Override
+    @Transactional // Create,Delete, Update
     public EmailDto basicSendEmail(EmailDto emailDto) {
-        return null;
+        // Maili Öncelikle Kaydedin.
+        EmailDto firstDatabaseSave=mailDatabase(emailDto);
+
+        // Mail kaydettiğinde emin olana kadar mail gönderme
+        if(firstDatabaseSave.getId()!=null){
+            SimpleMailMessage simpleMailMessage=new SimpleMailMessage();
+            simpleMailMessage.setFrom(emailDto.getEmailFrom());// Kim gönderdi
+            simpleMailMessage.setTo(emailDto.getEmailTo());// Kime
+            simpleMailMessage.setSubject(emailDto.getEmailSubject());// Konu
+            simpleMailMessage.setText(emailDto.getEmailText());// Text
+            simpleMailMessage.setCc(emailDto.getEmailCc());// CC
+            simpleMailMessage.setBcc(emailDto.getEmailBcc());// BCC
+            simpleMailMessage.setSentDate(emailDto.getSystemCreatedDate());
+            System.out.println(simpleMailMessage.hashCode());
+
+            // Mail Gönder
+            javaMailSender.send(simpleMailMessage);
+            return emailDto;
+        }else{
+            throw new HamitMizrakException("Database kaydetmedi ve bundan dolayı gönderilmedi");
+        }
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////
@@ -66,4 +96,4 @@ public class EmailServicesImpl implements IEmailServices<EmailDto, EmailEntity> 
         return null;
     }
 
-}  //end EmailServicesImpl
+} //end EmailServicesImpl
